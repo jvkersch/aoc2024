fe86710116e413fc6c7d34f0c2ea2577780bfcc5
Joris Vankerschaver <joris.vankerschaver@gmail.com>
2024-12-06 07:23:20 +0900

    Day 4

--- day04.jl
+++ day04.jl
@@ -1,71 +1,29 @@
-gridlines = [
-    "MMMSXXMASM",
-    "MSAMXMSMSA",
-    "AMXSXMAAMM",
-    "MSAMASMSMX",
-    "XMASAMXAMM",
-    "XXAMMXXAMA",
-    "SMSMSASXSS",
-    "SAXAMASAAA",
-    "MAMMMXMMMM",
-    "MXMXAXMASX",
-]
-
-# gridlines = [
-#     "XUUU",
-#     "UMUU",
-#     "UUAU",
-#     "UUUS"
-# ]
-
-function make_grid(lines)
-    padding = repeat(".", length(lines[1]) + 2)
-    lines = [padding; ["." * line * "." for line ∈ lines]; padding]
-    hcat(map(collect, lines)...)
+function make_grid(lines; padding=3)
+    inner = hcat(map(collect, lines)...)
+    rows, cols = size(inner)
+    grid = fill('.', rows + 2 * padding, cols + 2 * padding)
+    grid[padding+1:end-padding, padding+1:end-padding] = inner
+    grid
 end
 
 I = CartesianIndex
 OFFSETS::Vector{I} = [
     I(-1, -1), I(-1, 0), I(-1, 1), I(0, -1), I(0, 1), I(1, -1), I(1, 0), I(1, 1)
 ]
-neighbors(index) = index .+ OFFSETS
 
-grid = make_grid(gridlines)
-starts = findall(grid .== 'X')
+words_at(grid, c) = [join([grid[c + i * offset] for i ∈ 0:3]) for offset ∈ OFFSETS]
 
-
-function dfs(grid, path)
-    start = last(path)
-    paths = []
-    for n ∈ neighbors(start)
-        grid[n] != "MAS"[length(path)] && continue
-        if length(path) == 3
-            push!(paths, [path; n])
-        else
-            append!(paths, dfs(grid, [path; n]))
-        end
+function count_words(grid)
+    nwords = 0
+    for c ∈ CartesianIndices(grid)
+        grid[c] == 'X' || continue
+        nwords += sum(words_at(grid, c) .== "XMAS")
     end
-    return paths
+    nwords
 end
 
-function mark_paths(grid, paths)
-    npaths = 0
-    isempty = trues(size(grid))
-    for p ∈ paths
-        if any([isempty[n] for n ∈ p])
-            npaths += 1
-        end
-        for n ∈ p
-            isempty[n] = false
-        end
-    end
-    return npaths
+function part1()
+    lines = readlines("inputs/day04.txt")
+    grid = make_grid(lines)
+    println(count_words(grid))
 end
-
-
-all_paths = []
-for start ∈ starts
-    paths = dfs(grid, [start])
-    append!(all_paths, paths)
-end
-println(mark_paths(grid, all_paths))
