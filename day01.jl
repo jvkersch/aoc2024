4bac4cf5cc0b57463e855e1db4ce5e0346d964d8
Joris Vankerschaver <joris.vankerschaver@gmail.com>
2024-12-09 12:53:49 +0900

    Day 1

--- day01.jl
+++ day01.jl
@@ -4,5 +4,5 @@ data = readdlm("inputs/day01.txt", Int64)
 println(sum(abs.(sort(data[1:end, 1]) - sort(data[1:end, 2]))))
 
 counts = zeros(Int64, maximum(data))
-for el ∈ data[1:end, 2] counts[el] += 1
+for el ∈ data[1:end, 2] counts[el] += 1 end
 println(sum(data[1:end, 1] .* counts[data[1:end, 1]]))
