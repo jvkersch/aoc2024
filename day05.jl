d1ed44728db8de9e39c4bf65dbdc39ab5c4de7cc
Joris Vankerschaver <joris.vankerschaver@gmail.com>
2024-12-11 00:05:53 +0900

    Day 5

--- day05.jl
+++ day05.jl
@@ -1,17 +1,27 @@
-lines = readlines("inputs/day05.txt")
-n = findall(line -> line == "", lines)[1]
-rules = Set([parse.(Int, split(line, '|')) for line ∈ lines[1:n-1]])
-pagesets = [parse.(Int, split(line, ',')) for line ∈ lines[n+1:end]]
-
-invalid::Vector{Vector{Int}} = []
-middle_pages::Int = 0
-for pageset ∈ pagesets
-    global middle_pages
-    isvalid = all([[s, t] ∈ rules for (s, t) ∈ zip(pageset[1:end-1], pageset[2:end])])
-    isvalid && (middle_pages += pageset[(begin+end)÷2])
-    !isvalid && push!(invalid, pageset)
+function read_data()
+    lines = readlines("inputs/day05.txt")
+    n = first(findall(line -> line == "", lines))
+    rules = Set([parse.(Int, split(line, '|')) for line ∈ lines[1:n-1]])
+    pagesets = [parse.(Int, split(line, ',')) for line ∈ lines[n+1:end]]
+    return pagesets, rules
 end
-println(middle_pages)
+
+isvalid(pageset, rules) = all([[s, t] ∈ rules for (s, t) ∈ zip(pageset[1:end-1], pageset[2:end])])
+
+function partition_pagesets(pagesets, rules)
+    valid = similar(pagesets, 0)
+    invalid = similar(pagesets, 0)
+    for ps ∈ pagesets
+        push!(isvalid(ps, rules) ? valid : invalid, ps)
+    end
+    valid, invalid
+end
+
+function part1(pagesets, rules)
+    valid, _ = partition_pagesets(pagesets, rules)
+    middle_pages = sum([pageset[(begin+end)÷2] for pageset ∈ valid])
+    println(middle_pages)
+end
 
 restrict(nodes, rules) = filter(r -> (r[1] ∈ nodes) && (r[2] ∈ nodes), rules)
 
@@ -26,6 +36,16 @@ function makegraph(rules)
 end
 
 function toposort(graph)
+
+    function dfs(graph, start, visited, processed)
+        push!(visited, start)
+        for neigh ∈ graph[start]
+            neigh ∈ visited && continue
+            dfs(graph, neigh, visited, processed)
+        end
+        push!(processed, start)
+    end
+
     visited = Set()
     processed = Vector{Int}()
     for vertex ∈ keys(graph)
@@ -34,20 +54,22 @@ function toposort(graph)
     reverse(processed)
 end
 
-function dfs(graph, start, visited, processed)
-    push!(visited, start)
-    for neigh ∈ graph[start]
-        neigh ∈ visited && continue
-        dfs(graph, neigh, visited, processed)
+function part2(pagesets, rules)
+    valid, invalid = partition_pagesets(pagesets, rules)
+
+    middle_pages_fixed = 0
+    for pageset ∈ invalid
+        g = makegraph(restrict(pageset, rules))
+        fixed = toposort(g)
+        middle_pages_fixed += fixed[(begin+end)÷2]
     end
-    push!(processed, start)
+    println(middle_pages_fixed)
 end
 
-middle_pages_fixed::Int = 0
-for pageset ∈ invalid
-    global middle_pages_fixed
-    local g = makegraph(restrict(pageset, rules))
-    fixed = toposort(g)
-    middle_pages_fixed += fixed[(begin+end)÷2]
+function main()
+    pagesets, rules = read_data()
+    part1(pagesets, rules)
+    part2(pagesets, rules)
 end
-println(middle_pages_fixed)
+
+main()
