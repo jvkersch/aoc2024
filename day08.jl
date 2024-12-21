1e1ab4f8217e2bd9c2fd72c2356dcd1a51f0a07e
Joris Vankerschaver <joris.vankerschaver@gmail.com>
2024-12-21 11:02:29 +0900

    Day 8

--- day08.jl
+++ day08.jl
@@ -17,10 +17,6 @@ function find_antinodes(grid, antennas)
     antinodes = Set{I}()
     for antennas_for_type ∈ values(antennas)
         for (a1, a2) in combinations(antennas_for_type, 2)
-            if 2*a1-a2 == I(1,1) || 2*a2-a1 == I(1, 1)
-                println("here", a1, " ", a2)
-            end
-
             push!(antinodes, 2*a2 - a1)
             push!(antinodes, 2*a1 - a2)
         end
@@ -28,18 +24,36 @@ function find_antinodes(grid, antennas)
     filter(idx -> checkbounds(Bool, grid, idx), antinodes)
 end
 
-function mark_antinodes(grid, antinodes)
-    grid = copy(grid)
-    grid[[node for node ∈ antinodes]] .= '#'
-    grid
+function find_antinodes_resonant(grid, antennas)
+    antinodes = Set{I}()
+    for antennas_for_type ∈ values(antennas)
+        for (a1, a2) ∈ combinations(antennas_for_type, 2)
+            delta = a2 - a1
+            n2 = a2 + delta
+            while checkbounds(Bool, grid, n2)
+                push!(antinodes, n2)
+                n2 += delta
+            end
+            n1 = a1 + delta
+            while checkbounds(Bool, grid, n1)
+                push!(antinodes, n1)
+                n1 -= delta
+            end
+        end
+    end
+    antinodes
 end
 
 function part1()
     grid = readdata()
     antennas = find_antennas(grid)
     antinodes = find_antinodes(grid, antennas)
-    #mark_antinodes(grid, antinodes)
     println(length(antinodes))
 end
 
-part1()
+function part2()
+    grid = readdata()
+    antennas = find_antennas(grid)
+    antinodes = find_antinodes_resonant(grid, antennas)
+    println(length(antinodes))
+end
